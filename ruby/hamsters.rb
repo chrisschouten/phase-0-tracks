1afe997e109104bee3eda571567ba21adb7ea731
Chris Schouten <christopheraschouten@gmail.com>
2016-03-26 12:43:03 -0500

    Complete release 1

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,3 +2,9 @@ puts "Hello little critter, what is your name?"
 ham_name = gets.chomp
 puts "Hi #{ham_name}, what is your level or snoring on a scale 1-10?"
 snore_level = gets.chomp
+puts "Okay #{ham_name}, what color are you?"
+color = gets.chomp
+puts "Are you a good candidate for adoption?"
+candidate = gets.chomp
+puts "What is your age?"
+age = nil
