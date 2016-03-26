722110b26fc2ab82f73f5c7046143964da0c6215
Chris Schouten <christopheraschouten@gmail.com>
2016-03-26 14:32:43 -0500

    Complete 4.3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,10 +1,32 @@
 puts "Hello little critter, what is your name?"
 ham_name = gets.chomp
-puts "Hi #{ham_name}, what is your level or snoring on a scale 1-10?"
+puts "Hi #{ham_name}, what is your level of snoring on a scale 1-10?"
 snore_level = gets.chomp
+snore_level.to_i
 puts "Okay #{ham_name}, what color are you?"
 color = gets.chomp
-puts "Are you a good candidate for adoption?"
+puts "Are you a good candidate for adoption? (y/n)"
 candidate = gets.chomp
+
+
+
 puts "What is your age?"
-age = nil
+age = gets.chomp
+age.to_i
+
+puts "So let me confirm what you've told me.  Your name is #{ham_name}."
+puts "Your snoring level is #{snore_level}."
+puts "Your color is #{color} and your age is #{age}"
+
+if candidate == "y"
+	adoptible = true
+else 
+	adoptible = false
+end
+
+if adoptible
+	puts "Congratulations!  You are adoptible!"
+elsif
+	puts "Sorry:(  You are not adoptible"
+end
+puts "Have a nice day #{ham_name}!"
