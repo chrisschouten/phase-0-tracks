fa414efbeb86ee3f4505adc1d7a93ed41a51faaa
Chris Schouten <christopheraschouten@gmail.com>
2016-03-31 11:57:32 -0500

    Create vampire questions.

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were your born?"
+year_of_birth = gets.chomp
+
+puts "Our company cafeteria serves garlic bread.  Should we order some for you?"
+likes_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
