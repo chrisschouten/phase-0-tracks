297b4e6bafa8caf8f45f4b0459849710122c4714
Chris Schouten <christopheraschouten@gmail.com>
2016-03-31 18:41:31 -0500

    Finish 4.4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,9 @@
+puts "How many employees will be taking this survey?"
+employees_surveyed = gets.chomp
+
+	all_employees = 0
+until all_employees == employees_surveyed.to_i
+
 puts "What is your name?"
 name = gets.chomp
 
@@ -12,6 +18,17 @@ likes_garlic = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
+
+done = false
+until done == "done"
+		puts "List your alergies."
+		done = gets.chomp
+		if done == "sunshine"
+		puts "Probably a vampire."
+		break
+	end
+end
+
 
 if age.to_i >= 200
 	valid_age = true
@@ -33,20 +50,19 @@ end
 	end
 
 	if name == "Drake Cula"
-		vampire = true
+		puts "Definitely a vampire."
 	elsif name == "Tu Fang"
-		vampire = true
-	else
-		vampire = false
-	end
-if valid_age && (garlic_eater || needs_insurance)
-	puts "Probably not a vampire."
-elsif !valid_age && (!garlic_eater || !needs_insurance)
-	puts "Probably a vampire."
-elsif !valid_age && !garlic_eater && !needs_insurance
-	puts "Almost certainly a valid_age."
-elsif name == "Drake Cula"  || "Tu Fang"
-	puts "Definitely a vampire"
+		puts "Definitely a vampire"
+	elsif valid_age && (garlic_eater || needs_insurance)
+		puts "Probably not a vampire."
+	elsif !valid_age && (!garlic_eater || !needs_insurance)
+		puts "Probably a vampire."
+	elsif !valid_age && !garlic_eater && !needs_insurance
+		puts "Almost certainly a valid_age."
 	else
 		puts "Results inconclusive"
 	end
+
+	all_employees += 1
+end
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends." 
