c456a8af66e4f8460dfa7cfc130fc14da9bb8601
Chris Schouten <christopheraschouten@gmail.com>
2016-03-31 16:16:29 -0500

    Add conditionals

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,3 +12,41 @@ likes_garlic = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
+
+if age.to_i >= 200
+	valid_age = true
+
+else
+	valid_age = false
+end
+
+	if likes_garlic == "y"
+		garlic_eater = true
+		else
+			garlic_eater = false
+		end
+
+	if health_insurance == "y"
+		needs_insurance = true
+	else
+		needs_insurance= false
+	end
+
+	if name == "Drake Cula"
+		vampire = true
+	elsif name == "Tu Fang"
+		vampire = true
+	else
+		vampire = false
+	end
+if valid_age && (garlic_eater || needs_insurance)
+	puts "Probably not a vampire."
+elsif !valid_age && (!garlic_eater || !needs_insurance)
+	puts "Probably a vampire."
+elsif !valid_age && !garlic_eater && !needs_insurance
+	puts "Almost certainly a valid_age."
+elsif name == "Drake Cula"  || "Tu Fang"
+	puts "Definitely a vampire"
+	else
+		puts "Results inconclusive"
+	end
