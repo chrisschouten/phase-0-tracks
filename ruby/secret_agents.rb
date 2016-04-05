da10aa1fce6e0a1296a5b70e2c169545c2ba5e83
Chris Schouten <christopheraschouten@gmail.com>
2016-04-05 17:32:53 -0500

    Add code

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,19 @@
+
+	
+	index = 0
+	add_letter ="abc"
+	# declare emplty string to add one letter to 
+	
+	#loop over the string's letters
+	while index < add_letter.length
+		add_letter[index] = add_letter[index].next!
+		index += 1
+	end
+
+	puts add_letter
+ 
+ 
+
+	
+
+
