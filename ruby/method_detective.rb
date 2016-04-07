f15d4b92314ef03d17fd98810b0b76986c2f556d
Chris Schouten <christopheraschouten@gmail.com>
2016-04-07 16:57:08 -0500

    Complete 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,57 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
-# => “InVeStIgAtIoN”
+ a = "InVEStiGaTiON".swapcase
+ puts a
 
-# “zom”.<???>
-# => “zoom”
+ # => “InVeStIgAtIoN”
 
-# “enhance”.<???>
+ b ="zom".insert(2, "o")
+puts b # => “zoom”
+
+c = "enhance".center(20)
+puts c
 #=> “    enhance    ”
 
-# “Stop! You’re under arrest!”.<???>
+d = "Stop! You are under arrest!!".upcase
+puts d 
 # => “STOP! YOU’RE UNDER ARREST!”
 
-# “the usual”.<???>
+e = "the usual "
+e << "suspects"
+puts e
+
+
+
+f = "suspects"
+ f.prepend("the usual ")
+ puts f
 #=> “the usual suspects”
 
-#“ suspects”.<???>
-#=> “the usual suspects”
-
-# “The case of the disappearing last letter”.<???>
+ g = "The case of the disappearing last letter".chop
+ puts g
 # => “The case of the disappearing last lette”
 
-# “The mystery of the missing first letter”.<???>
+h = "The mystery of the missing first letter"
+h.slice!(0)
+puts h
+ 
+i = "The case of the missing first letter"[1..-1]
+ 
+puts i
+
 # => “he mystery of the missing first letter”
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
+x =  "Elementary  ,    my   dear      Watson!".squeeze
+puts x
 # => “Elementary, my dear Watson!”
 
-#“z”.<???>
+z ="z".ord
+puts z
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# the program has assigned a value to the variable "z" of 122
+s = "How ,many times does the letter a appear in this string".count "a"
+puts s
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
 #=> 4
