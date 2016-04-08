0d7a6d8fbd9e70a1a3df5cad7beb087e56732104
Chris Schouten <christopheraschouten@gmail.com>
2016-04-07 20:36:34 -0500

    Array assignment

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,25 @@
+classic_rock = ["Who", "Beatles", "Stones", "Pink Flyod", "Queen"]
+p classic_rock
+
+classic_rock.delete_at(2)
+p classic_rock
+
+classic_rock.insert(2, "Stones")
+p classic_rock
+
+
+classic_rock.delete"Who"
+p classic_rock
+
+
+	classic_rock = 
+
+	if classic_rock.include?("Who")
+		puts "Yes, they are here"
+	else
+		puts "No, they are not here"
+	end
+
+
+
+ 
