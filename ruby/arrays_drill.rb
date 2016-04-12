54a5a6e1d4dca061577ef4a1abb0133d5df71081
Chris Schouten <christopheraschouten@gmail.com>
2016-04-12 14:51:45 -0500

    Complete assignment

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,8 @@
+def add_to_array(arr, new1, new2, new3, new4)
+	last_array = "#{arr} #{new1} #{new2} #{new3} #{new4}".split
+end
+
+
 def build_array(arg1, arg2, arg3)
 	array_new = " #{arg1} #{arg2} #{arg3}".split
 end
@@ -31,8 +36,26 @@ puts "what band would you like to check"
 band = gets.chomp
 checker(band)
 
-array_new = build_array(5, "two", nil)
-puts array_new
+array_new = build_array(1, "two", true)
+p array_new
+
+arr = 'cat dog birds true'
+last = add_to_array(arr, "fjf", "rururu", 8548, "tutut")
+p last
+
+def c_array(fjfj, uuu)
+	c_array = [1, 2, 3]
+	c_array = c_array << "#{fjfj} #{uuu}"
+end
+
+yyyyy = c_array("a", true)
+p yyyyy
+
+def b_array(listx, listy)
+		ddd = ["#{listx}" << "#{listy}"].flatten
 
 
 
+	end
+
+p b_array(["a", "b", "c", 1, 2], 3)
