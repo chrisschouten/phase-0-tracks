1a611d395fb896ed7bfacafb588f6cd72137e5e8
Chris Schouten <christopheraschouten@gmail.com>
2016-04-16 11:46:08 -0500

    Complete nested data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,14 @@
+solar_system = {
+	"Mars" => ["red", "hot", "rocky"],
+	"Venus" => ["green", "really hot", "dusty"],
+	"Mercury"=> ["brown", "very hot", "barren"],
+	"Earth" => ["blue", "pleasant", "oceans"],
+	"Jupiter" => ["Orange", "warm", "gassy"],
+}
+p solar_system
+
+p solar_system["Mars"][1]
+
+p solar_system["Earth"][2]
+
+p solar_system["Jupiter"][0..2]
