3fbaa2db8b9678a14c74bf5c713ca932dcb69f8b
Chris Schouten <christopheraschouten@gmail.com>
2016-04-21 19:57:42 -0500

    Finish iterations methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,70 @@
+def hello
+  puts "Hello World"
+  3.times { yield("3") }
+end
+
+hello { |count| puts "This is our new method #{count}." }
+
+# # In phase-0-tracks/ruby/iteration.rb, declare an array and a hash, and populate each of them with some data. We don't care what data you use, but it shouldn't be nonsense data -- it should represent a reasonable scenario in which those data structures would be used. (A hash full of random letters and numbers, for instance, is not as meaningful as a hash that associates Hollywood actors with their best-known character.)
+
+# # Demonstrate that you can iterate through each one using .each, and then using .map! (modifying the data in some way). Note that you can't call .map! on a hash, so you can skip that (.map returns an array, so a destructive map method is logically impossible on a hash). Print the data structures before and after each call to demonstrate whether they have been modified or not.
+
+names = ["john", "harry", "benjamin"]
+food_types = {
+  fruit: "banana",
+  vegetable: "lettuce",
+  nuts: "peanuts",
+  meat: "beef"
+  }
+
+puts "before .each call"
+p names
+p food_types
+puts "after .each call"
+capitalized_names = []
+names.each do |name|
+  capitalized_names << name.capitalize
+end
+p capitalized_names
+
+food_types.each do |category, food|
+  puts "A type of #{category} is #{food}."
+end
+
+puts "after .map!"
+
+names.map! do |name|
+  name.capitalize
+end
+
+p names
+
+int = [ 1, 2, 3, 4, 5, 6]
+# delete_if { |item| block } → ary
+int.delete_if { |integer| integer < 5 }
+p int
+
+int = [ 1, 2, 3, 4, 5, 6]
+int.keep_if { |integer| integer < 5 }
+p int
+
+int = [ 1, 2, 3, 4, 5, 6]
+int.keep_if { |integer| integer.odd? }
+p int 
+
+
+# Use the documentation to find other Array and Hash methods that take blocks, and that can accomplish the tasks below, then add sample calls to phase-0-tracks/ruby/iteration.rb -- one for the array, and one for the hash.
+
+# You can use simple (nonmeaningful) data for your example calls -- an array of integers or letters would be fine, for instance.
+
+# A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
+# A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+# A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
+
+
+
+
+
 def my_method
 	puts "This is awesome!"
 	yield("Ruby", "HTML")
--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -13,3 +13,19 @@ p solar_system["Mars"][1]
 p solar_system["Earth"][2]
 
 p solar_system["Jupiter"][0..2]
+
+
+
+couples = Array.new
+
+couples.push(["mom", "dad"], ["brother", "sister"], ["aunt", "uncle"])
+p couples
+
+couples[1].push("little brother")
+p couples
+
+couples[2].push([cousin: "Brian"])
+p couples
+
+p couples[2][2][0][:cousin]
+p couples[2][2][0].key("Brian")
