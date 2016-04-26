1008c69fcb3ccc820b5fa4574db193a0a12a6332
Chris Schouten <christopheraschouten@gmail.com>
2016-04-26 12:52:59 -0500

    complete week 5 assesment

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,38 +5,38 @@
 # # iterate over data to change all vowels to the next vowel in aeiou
 # # and change all consonants to the next consonants
 
-puts "What is your first and last name?"
-name  = gets.chomp #capture name from user input
+# puts "What is your first and last name?"
+# name  = gets.chomp #capture name from user input
 
-#downcase name
-name_downcase = name.downcase
+# #downcase name
+# name_downcase = name.downcase
 
-# transforms the alphabet using the transform method.
-shift_letter = name_downcase.tr('aeioubcdfghjklmnpqrstvwxyz', 'eiouacdfghjklmnpqrstvwxyzb')
+# # transforms the alphabet using the transform method.
+# shift_letter = name_downcase.tr('aeioubcdfghjklmnpqrstvwxyz', 'eiouacdfghjklmnpqrstvwxyzb')
 
-# transform string into an array
-name_to_array = shift_letter.split(" ")
+# # transform string into an array
+# name_to_array = shift_letter.split(" ")
 
-# capitalize each word in string
-name_is_capitalized = name_to_array.map do |alias_name|
-	alias_name.capitalize
-end
+# # capitalize each word in string
+# name_is_capitalized = name_to_array.map do |alias_name|
+# 	alias_name.capitalize
+# end
 
-# swap order of first and last name 
-name_is_reversed = name_is_capitalized.reverse
+# # swap order of first and last name 
+# name_is_reversed = name_is_capitalized.reverse
 
 
-# rejoin array in string
-final_alias = name_is_reversed.join(" ")
+# # rejoin array in string
+# final_alias = name_is_reversed.join(" ")
 
-def alias_roster(name, final_alias)
-	puts "#{name}, your alias name is #{final_alias}."
+# def alias_roster(name, final_alias)
+# 	puts "#{name}, your alias name is #{final_alias}."
 	
-end
+# end
 
-name_is_reversed.each do |item|
-	puts "#{name}, your alisas name is #{final_alias}."
-end
+# name_is_reversed.each do |item|
+# 	puts "#{name}, your alisas name is #{final_alias}."
+# end
 
 
 
--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -11,11 +11,37 @@ def calc(int1, operator, int2)
   end
 end
 # code to test calc method
-# calc(4, "+", 2)
-# calc(4, "-", 2)
-# calc(4, "*", 2)
-# calc(4, "/", 2)
+calc(4, "+", 2)
+calc(4, "-", 2)
+calc(4, "*", 2)
+calc(4, "/", 2)
 
-# create container has
-calc_hist = Hash.new
-p calc_hist
+# initialize storage container for answers to calculations
+calculations = {}
+
+response = ""
+# loop until response == 'done'
+until response == 'done'
+# ask for user input
+puts "What would you like to calculate?(or type 'done' to exit)."
+response = gets.chomp
+
+# array contructed from user input
+array = response.split(" ")
+int1= array[0].to_i
+operator = array[1]
+int2 = array[2].to_i
+
+
+answer = calc(int1, operator, int2)
+    # collects data into hash for user output
+  calculations[response] = answer
+   answer
+  end
+
+  puts "#{calculations.keys.length-1} calculations performed."
+   # iterates thru hash and prints output for each calculation
+  calculations.each do |calc, answer|
+  p "#{calc}: #{answer}"
+
+end
