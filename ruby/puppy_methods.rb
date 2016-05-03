4b30280b75ed8ad0a842fe99f34897cde83b9130
Chris Schouten <christopheraschouten@gmail.com>
2016-05-02 20:40:38 -0500

    Add methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,36 @@ class Puppy
     toy
   end
 
+
+  def speak(int)
+		puts "Woof!" * int
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(age)
+  	age * 7
+  end
+
+  def who_is_a_good_boy(name)
+  	puts "Who's a good boy? #{name.capitalize} is a good boy!"
+  end
 end
 
+
+
+
+
+
+
+
+
+
+sparky = Puppy.new
+sparky.who_is_a_good_boy("sparky")
+p sparky.dog_years(2)
+p sparky.fetch("ball") 
+p sparky.speak(5)
+p sparky.roll_over
