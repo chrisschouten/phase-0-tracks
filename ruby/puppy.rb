da9fa874ba4186ddcf365abcd6055626d36acb48
Chris Schouten <christopheraschouten@gmail.com>
2016-05-02 19:56:10 -0500

    Add comments for Puppy.class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Species
+# Newfoundland
+
+# Characteristics-------
+  # Fur length: long
+  # Eyes: doughy
+  # Paws: large
+  # name: varies
+  # color: black
+
+  # Behavior
+    # run
+    # lick
+    # chase
+    # play
