99aa68b2fc28833406fb8aae8a3fa209bb6f7f5e
Chris Schouten <christopheraschouten@gmail.com>
2016-05-05 00:20:04 -0500

    Add loop and user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,7 +1,7 @@
 class Music
 
-	attr_reader :play, :title
-	attr_writer :rating, :volume
+	attr_reader :category, :title, :volume, :rating
+	attr_accessor 
 
 
 	def initialize(category, title, volume)
@@ -21,23 +21,41 @@ class Music
 	end
 
 	def suggest_song_to_play(song)
-		puts "I would love the play '#{song}' for you!"
-	end
-
-	def loudness
-		@loudness
-	end 
-
-	def loudness=(new_volume)
-		@loudness = new_volume
-	end
-		
+		puts "I would love to play '#{song}' for you!"
+	end	
 end
 
-song = Music.new("country", "whatever", 10)
-song.loudness
+play_list = Array.new
+loop do
+  puts "Let's create a play list.  Enter in the song title."
+  title = gets.chomp
+  puts "Enter in the music category."
+  category = gets.chomp
+  puts "Enter in the rating."
+  rating = gets.chomp
 
-song.loudness = 5
-puts "The new volume for #{song.title} is: #{song.loudness}."
+  songs = []
+  songs << song = Music.new(title, category, rating)
+
+  songs.each do |item|
+  	play_list << song.title
+  	play_list << song.category
+  	play_list << song.rating
+  	play_list << song.volume
+  end
+  puts play_list
+  
+  
+
+  puts "Would you like to continue to add to your play list? (y/n)"
+  ans = gets.chomp
+  if ans == "n"
+	  break
+  end
+end
+
+p play_list
+
+
 
 
