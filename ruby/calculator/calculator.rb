983ab293ab9b9009ee2fd19a394b8142de271a01
Chris Schouten <christopheraschouten@gmail.com>
2016-05-05 20:14:37 -0500

    Add divide method

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,15 +1,19 @@
-# class Calculator
-#   def add(x,y)
-#     x + y
-#   end
+class Calculator
+  def add(x,y)
+    x + y
+  end
 
-#   def subtract(x,y)
-#     x - y
-#   end
+  def subtract(x,y)
+    x - y
+  end
 
-#   def multiply(x,y)
-#     x * y
-#   end
-# end
+  def multiply(x,y)
+    x * y
+  end
+
+  def divides(x,y)
+  	x / y
+  end
+end
 
 puts "hello there"
