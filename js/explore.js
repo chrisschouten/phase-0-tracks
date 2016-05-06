f85027b05b9f3ff22e2a6caa6e2df3cdba706bf7
Chris Schouten <christopheraschouten@gmail.com>
2016-05-06 13:24:15 -0500

    Add reverse funtion

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// This function will take //String and reverse each letter
+// In comments, pseudocode // function that takes a string as a parameter and reverses the string.
+// reverse("hello"), for example, should return "olleh". 
+//This isn't a task specific to JavaScript -- your pseudocode should be in plain English and use phrases like "for each" instead of JavaScript-specific terms. 
+//Remember that you can add strings in JavaScript, so '' + 'a' would result in the string 'a'. 
+
+var x = function reverse(word) {
+	return word.split('').reverse().join('');
+}
+
+console.log(x("hello"))
+
+
+var b = 1 == 1
+console.log(b)
